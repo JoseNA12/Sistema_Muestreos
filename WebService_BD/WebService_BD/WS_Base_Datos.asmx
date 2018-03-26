b426ff92468c7f6d4499400938b47b450f53ff3d
José Navarro <josenava@estudiantec.cr>
2018-03-26 15:13:26 -0600

    Commit inicial

---
+++ WebService_BD/WebService_BD/WS_Base_Datos.asmx
@@ -0,0 +1,1 @@
+﻿<%@ WebService Language="C#" CodeBehind="WS_Base_Datos.asmx.cs" Class="WebService_BD.WS_Base_Datos" %>
---
+++ WebService_BD/WebService_BD/WS_Base_Datos.asmx.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient; //Para la conexión con la base datos
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+
+namespace WebService_BD
+{
+    /// <summary>
+    /// Descripción breve de WS_Base_Datos
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    [System.ComponentModel.ToolboxItem(false)]
+    // Para permitir que se llame a este servicio web desde un script, usando ASP.NET AJAX, quite la marca de comentario de la línea siguiente. 
+    // [System.Web.Script.Services.ScriptService]
+    public class WS_Base_Datos : System.Web.Services.WebService
+    {
+
+        [WebMethod]
+        public string HelloWorld()
+        {
+            return "Hola a todos";
+        }
+
+        [WebMethod]
+        public DataSet GetData()
+        {
+            SqlConnection conn = new SqlConnection();
+            conn.ConnectionString = "Data Source =NOMBRE_SERVIDOR; Intial Catalog=NOMBRE_BD; Integrated Security=True;";
+            SqlDataAdapter da = new SqlDataAdapter("SELECT * FROM NOMBRE_TABLA", conn);
+            DataSet ds = new DataSet();
+            da.Fill(ds);
+            return ds;
+        }
+    }
+}
